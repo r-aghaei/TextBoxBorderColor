d0d9f3dc78c43253a82d65d97167e0d0cc3bf62d
Reza Aghaei <rag@sitecore.net>
2017-12-03 15:00:32 +0800

    Added MyTextBox and Sample Form.

--- TextBoxBorderColor/Form1.Designer.cs
+++ TextBoxBorderColor/Form1.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.myTextBox1 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox2 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox3 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox4 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox5 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox6 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox7 = new TextBoxBorderColor.MyTextBox();
+            this.myTextBox8 = new TextBoxBorderColor.MyTextBox();
+            this.SuspendLayout();
+            // 
+            // myTextBox1
+            // 
+            this.myTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox1.BorderColor = System.Drawing.Color.Red;
+            this.myTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.myTextBox1.Name = "myTextBox1";
+            this.myTextBox1.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox1.TabIndex = 0;
+            this.myTextBox1.Text = "Red";
+            // 
+            // myTextBox2
+            // 
+            this.myTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox2.BorderColor = System.Drawing.Color.Red;
+            this.myTextBox2.Enabled = false;
+            this.myTextBox2.Location = new System.Drawing.Point(12, 49);
+            this.myTextBox2.Name = "myTextBox2";
+            this.myTextBox2.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox2.TabIndex = 0;
+            this.myTextBox2.Text = "Red Disabled";
+            // 
+            // myTextBox3
+            // 
+            this.myTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox3.BorderColor = System.Drawing.Color.Green;
+            this.myTextBox3.Location = new System.Drawing.Point(12, 86);
+            this.myTextBox3.Name = "myTextBox3";
+            this.myTextBox3.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox3.TabIndex = 0;
+            this.myTextBox3.Text = "Green";
+            // 
+            // myTextBox4
+            // 
+            this.myTextBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox4.BorderColor = System.Drawing.Color.Blue;
+            this.myTextBox4.Location = new System.Drawing.Point(12, 123);
+            this.myTextBox4.Name = "myTextBox4";
+            this.myTextBox4.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox4.TabIndex = 0;
+            this.myTextBox4.Text = "Blue";
+            // 
+            // myTextBox5
+            // 
+            this.myTextBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox5.BorderColor = System.Drawing.Color.Transparent;
+            this.myTextBox5.Location = new System.Drawing.Point(12, 160);
+            this.myTextBox5.Name = "myTextBox5";
+            this.myTextBox5.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox5.TabIndex = 0;
+            this.myTextBox5.Text = "Theme Border Color";
+            // 
+            // myTextBox6
+            // 
+            this.myTextBox6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox6.BorderColor = System.Drawing.Color.Transparent;
+            this.myTextBox6.Enabled = false;
+            this.myTextBox6.Location = new System.Drawing.Point(12, 197);
+            this.myTextBox6.Name = "myTextBox6";
+            this.myTextBox6.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox6.TabIndex = 0;
+            this.myTextBox6.Text = "Theme Border Color - Disabled";
+            // 
+            // myTextBox7
+            // 
+            this.myTextBox7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox7.BorderColor = System.Drawing.Color.Blue;
+            this.myTextBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myTextBox7.Location = new System.Drawing.Point(12, 234);
+            this.myTextBox7.Name = "myTextBox7";
+            this.myTextBox7.Size = new System.Drawing.Size(254, 26);
+            this.myTextBox7.TabIndex = 0;
+            // 
+            // myTextBox8
+            // 
+            this.myTextBox8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myTextBox8.BorderColor = System.Drawing.Color.Blue;
+            this.myTextBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.myTextBox8.Location = new System.Drawing.Point(12, 271);
+            this.myTextBox8.Name = "myTextBox8";
+            this.myTextBox8.Size = new System.Drawing.Size(254, 19);
+            this.myTextBox8.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 307);
+            this.Controls.Add(this.myTextBox8);
+            this.Controls.Add(this.myTextBox7);
+            this.Controls.Add(this.myTextBox6);
+            this.Controls.Add(this.myTextBox5);
+            this.Controls.Add(this.myTextBox4);
+            this.Controls.Add(this.myTextBox3);
+            this.Controls.Add(this.myTextBox2);
+            this.Controls.Add(this.myTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MyTextBox myTextBox1;
+        private MyTextBox myTextBox2;
+        private MyTextBox myTextBox3;
+        private MyTextBox myTextBox4;
+        private MyTextBox myTextBox5;
+        private MyTextBox myTextBox6;
+        private MyTextBox myTextBox7;
+        private MyTextBox myTextBox8;
     }
 }
 
---
+++ TextBoxBorderColor/MyTextBox.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TextBoxBorderColor
+{
+    public class MyTextBox : TextBox
+    {
+        const int WM_NCPAINT = 0x85;
+        const uint RDW_INVALIDATE = 0x1;
+        const uint RDW_IUPDATENOW = 0x100;
+        const uint RDW_FRAME = 0x400;
+        [DllImport("user32.dll")]
+        static extern IntPtr GetWindowDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        [DllImport("user32.dll")]
+        static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprc, IntPtr hrgn, uint flags);
+        Color borderColor = Color.Blue;
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                RedrawWindow(Handle, IntPtr.Zero, IntPtr.Zero,
+                    RDW_FRAME | RDW_IUPDATENOW | RDW_INVALIDATE);
+            }
+        }
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCPAINT && BorderColor != Color.Transparent &&
+                BorderStyle == System.Windows.Forms.BorderStyle.Fixed3D)
+            {
+                var hdc = GetWindowDC(this.Handle);
+                using (var g = Graphics.FromHdcInternal(hdc))
+                using (var p = new Pen(BorderColor))
+                    g.DrawRectangle(p, new Rectangle(0, 0, Width - 1, Height - 1));
+                ReleaseDC(this.Handle, hdc);
+            }
+        }
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            RedrawWindow(Handle, IntPtr.Zero, IntPtr.Zero,
+                   RDW_FRAME | RDW_IUPDATENOW | RDW_INVALIDATE);
+        }
+    }
+}
